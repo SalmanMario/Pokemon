c3b23041c75cf84bb989a9919d9900625aab000e
Mario <xmariosalman@gmail.com>
2023-05-17 16:47:25 +0300

    pls work netlify ffs

--- src/pages/ViewPokemon.tsx
+++ src/pages/ViewPokemon.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { PokemonById, PokemonSpecies } from "../services/PokemonCall";
 import { AppRouteParams } from "../routes";
 import { Box } from "@mui/material";
 import { ShowPokemonId } from "../components/ShowPokemonId";
 import { fetchAndParse } from "../services/fetchAndParse";
+import { PokemonById, PokemonSpecies } from "../services/PokemonCall";
 
 function recursiveChainMapping(chain: any, result = [] as any[]): any {
   // returneaza elementele trecute + chain-ul curent
